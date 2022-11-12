99261590b9f9afae491ad11ef0a01323b77ba6ee
Vmadhavan2310 <105116989+Vmadhavan2310@users.noreply.github.com>
2022-11-12 14:47:36 +0530

    app test changes

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import react from 'react'
+import {render,screen} from '@testing-library/react'
+import App from './App'
+
+
+describe('group test',()=>{
+  
+  it('verify get by role',()=>{
+    render(<App/>)
+    expect(screen.getByTitle('heading')).toBeInTheDocument()
+  })
+})
