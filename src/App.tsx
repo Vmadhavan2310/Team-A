07422dabd7872ccf7909b44c515b130323334bf3
Vmadhavan2310 <105116989+Vmadhavan2310@users.noreply.github.com>
2022-11-12 14:11:48 +0530

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+
+
+export default function App() {
+  return (
+    <>
+      <h1 title="heading">Learn React</h1>
+      <h2>new</h2>
+      <img src="/" alt="testImage" />
+      <label htmlFor="Select-education">Select your Qualification</label>
+      <select name="qualification" id="Select-education">
+        <option value="">Choose any degree</option>
+        <option value="b.e">B.E.,</option>
+        <option value="b.tech">B.TECH</option>
+        <option value="b.sc">B.SC</option>
+      </select>
+     <input type="text"  value='value'/>
+    </>
+  )
+}
